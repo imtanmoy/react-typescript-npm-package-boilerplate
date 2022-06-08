695b3adefc3c73cf5a7fda241a2ea13b6d198cb2
Tanmoy Banik <imtanmoybanik@gmail.com>
2022-06-08 21:28:59 +0600

    lint

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./index.scss";
-import "./style.css";
-interface Props {
-    user: string;
-}
-declare const Car: React.FC<Props>;
-export default Car;
-//# sourceMappingURL=index.d.ts.map
--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,25 @@
-import del from "rollup-plugin-delete";
-import typescript from "rollup-plugin-typescript2";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
-import analyze from "rollup-plugin-analyzer";
-import postcss from "postcss";
-import autoprefixer from "autoprefixer";
-import scss from "rollup-plugin-scss";
-import commonjs from "@rollup/plugin-commonjs";
-import pkg from "./package.json";
+import del from 'rollup-plugin-delete';
+import typescript from 'rollup-plugin-typescript2';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import url from '@rollup/plugin-url';
+import svgr from '@svgr/rollup';
+import analyze from 'rollup-plugin-analyzer';
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
+import scss from 'rollup-plugin-scss';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
 
-const input = "src/index.tsx";
+const input = 'src/index.tsx';
 const globals = {
-  react: "React",
-  "react-dom": "ReactDOM",
+  react: 'React',
+  'react-dom': 'ReactDOM',
 };
 
 const plugins = [
-  del({ targets: "dist/*", runOnce: true }),
+  del({ targets: 'dist/*', runOnce: true }),
   typescript({ useTsconfigDeclarationDir: true }),
   external(),
   resolve(),
@@ -31,6 +33,8 @@ const plugins = [
         .process(css)
         .then((result) => result.css),
   }),
+  url(),
+  svgr({ icon: true }),
 ];
 
 export default [
@@ -39,15 +43,15 @@ export default [
     output: [
       {
         file: pkg.main,
-        format: "cjs",
-        exports: "named",
+        format: 'cjs',
+        exports: 'named',
         sourcemap: true,
         globals,
       },
       {
         file: pkg.module,
-        format: "esm",
-        exports: "named",
+        format: 'esm',
+        exports: 'named',
         sourcemap: true,
         globals,
       },
